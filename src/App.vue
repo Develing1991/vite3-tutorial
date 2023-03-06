22aafa46be8cf51fb9b47b2aa78e613e18de11fa
Develing1991 <completed0728@gmail.com>
2023-03-06 10:22:16 +0900

    composition api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <template>
-	<div>hello</div>
+	<div>
+		<h2>반응형 메시지</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button @click="addReactiveMessage">메시지 추가</button>
+		<p>{{ normalMessage }}</p>
+		<button @click="addNormalMessage">메시지 추가</button>
+	</div>
 </template>
 
 <script>
-	export default {};
+	import { onBeforeMount, onMounted, ref, isRef } from 'vue';
+
+	// 논리적 관심사 컴포저블
+	export default {
+		setup() {
+			console.log('setup()');
+			const reactiveMessage = ref('hello');
+			const addReactiveMessage = () => {
+				reactiveMessage.value = reactiveMessage.value + '!';
+			};
+			let normalMessage = 'hello';
+			const addNormalMessage = () => {
+				normalMessage = normalMessage + '!';
+			};
+			console.log(isRef(reactiveMessage));
+			console.log(isRef(normalMessage));
+
+			onMounted(() => {
+				console.log('onMounted');
+			});
+			onBeforeMount(() => {
+				console.log('onBeforeMount');
+			});
+			return {
+				reactiveMessage,
+				normalMessage,
+				addReactiveMessage,
+				addNormalMessage,
+			};
+		},
+	};
 </script>
 
 <style lang="scss" scoped></style>

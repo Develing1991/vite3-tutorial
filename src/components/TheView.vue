3a364aeff10d4632635a30159ea3cb1226f39be0
Develing1991 <completed0728@gmail.com>
2023-03-06 22:16:13 +0900

    create SFC

---
+++ src/components/TheView.vue
@@ -0,0 +1,41 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<div class="row g-5">
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+				<div class="col col-4">
+					<AppCard></AppCard>
+				</div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+	import AppCard from './AppCard.vue';
+
+	export default {
+		components: {
+			AppCard,
+		},
+		setup() {
+			return {};
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>

dbbb08cf0b1e82c5c5575dfdb34e129934e44c54
Develing1991 <completed0728@gmail.com>
2023-03-06 13:13:53 +0900

    vfor

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
 	<div>
-		<h2 v-if="visible">hh</h2>
-		<h2 v-else>hh</h2>
-		<template v-if="visible">
-			<div>hello</div>
-			<div>hello</div>
-			<div>hello</div>
-		</template>
-		<div v-show="visible">zzz</div>
+		<ul v-for="(item, index) in itemsArray" :key="item.id">
+			<li v-if="item.id % 2 == 0">{{ item.message }} - {{ index }}</li>
+		</ul>
+		<!-- key, value 반대 -->
+		<ul v-for="(value, key, index) in itemsObject" :key="key">
+			<li>{{ key }} - {{ value }} - {{ index }}</li>
+		</ul>
 	</div>
 </template>
 
 <script>
-	import { ref } from 'vue';
+	import { reactive } from 'vue';
 
 	export default {
 		setup() {
-			const visible = ref(true);
-			return { visible };
+			const itemsArray = reactive([
+				{ id: 1, message: 'Java' },
+				{ id: 2, message: 'HTML' },
+				{ id: 3, message: 'CSS' },
+				{ id: 4, message: 'JavaScript' },
+			]);
+			const itemsObject = reactive({
+				title: '제목입니다.',
+				author: '홍길동',
+				publishedAt: '2019-07-08',
+			});
+			return {
+				itemsArray,
+				itemsObject,
+			};
 		},
 	};
 </script>

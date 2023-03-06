18087c18d426633a932d4c9712d5616e5f93d9e5
Develing1991 <completed0728@gmail.com>
2023-03-06 23:15:48 +0900

    props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,53 +1,72 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
-			<!-- {{ $style }} -->
-			<!-- <p class="card-text" :class="$style.red"> -->
-			<!-- <p class="card-text" :class="classes.red"> -->
+			{{ $props }}
+			<!-- <span class="badge bg-secondary">
+				{{ type === 'news' ? '뉴스' : '공지사항' }}
+			</span> -->
+			<span class="badge bg-secondary">
+				{{ typeName }}
+			</span>
+			<h5 class="card-title red mt-2">{{ title }}</h5>
 			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ contents }}
 			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<!-- <a v-if="isLike" href="#" class="btn btn-danger">좋아요</a>
+			<a v-else href="#" class="btn btn-primary">좋아요</a> -->
+			<a href="#" class="btn" :class="isLikeClass" @click="toggleLike"
+				>좋아요</a
+			>
 		</div>
 	</div>
 </template>
 
 <script>
-	import { useCssModule, ref } from 'vue';
+	import { computed } from 'vue';
 
-	console.log('AppCard module');
 	export default {
-		setup() {
-			// const style = useCssModule();
-			// console.log(style);
-			// console.log('AppCard setup');
-			const color = ref('red');
-			return { color };
+		// props: ['title', 'contents'],
+		props: {
+			type: {
+				type: String,
+				default: 'news',
+				validator: value => {
+					return ['news', 'notice'].includes(value);
+				},
+			},
+			title: {
+				type: String,
+				required: true,
+			},
+			contents: {
+				type: String,
+				required: true,
+			},
+			isLike: {
+				type: Boolean,
+				required: false,
+			},
+			obj: {
+				type: Object,
+				default: () => ({}),
+			},
+		},
+		emits: ['toggleLike'],
+		setup(props, context) {
+			console.log(props.title);
+
+			const isLikeClass = computed(() => {
+				return props.isLike ? 'btn-danger' : 'btn-primary';
+			});
+			const typeName = computed(() =>
+				props.type === 'new' ? '뉴스' : '공지사항',
+			);
+			const toggleLike = () => {
+				context.emit('toggleLike');
+			};
+			return { isLikeClass, typeName, toggleLike };
 		},
 	};
 </script>
 
-<style scoped>
-	.red {
-		color: v-bind(color);
-	}
-</style>
-<!-- classes.red -->
-<!-- <style module="classes">
-	.red {
-		color: red;
-	}
-</style> -->
-<!-- $style.red -->
-<!-- <style module>
-	.red {
-		color: red;
-	}
-</style> -->
-<!-- <style scoped>
-	.red {
-		color: red;
-	}
-</style> -->
+<style lang="scss" scoped></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -3,22 +3,16 @@
 		<div class="container py-4">
 			<div class="row g-5">
 				<div class="col col-4">
-					<AppCard></AppCard>
+					<AppCard title="제목" :contents="contents"></AppCard>
 				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
-				</div>
-				<div class="col col-4">
-					<AppCard></AppCard>
+				<div v-for="item in posts" :key="item.title" class="col col-4">
+					<AppCard
+						:title="item.title"
+						:contents="item.contents"
+						:type="item.type"
+						:is-like="item.isLike"
+						@toggleLike="toggleLike(item)"
+					></AppCard>
 				</div>
 			</div>
 		</div>
@@ -26,6 +20,7 @@
 </template>
 
 <script>
+	import { ref, reactive } from 'vue';
 	import AppCard from './AppCard.vue';
 
 	export default {
@@ -33,7 +28,18 @@
 			AppCard,
 		},
 		setup() {
-			return {};
+			const contents = ref('내용');
+			const posts = reactive([
+				{ title: '제목', contents: '내용1', type: 'news', isLike: true },
+				{ title: '제목2', contents: '내용2', type: 'notice', isLike: true },
+				{ title: '제목3', contents: '내용3', type: 'news', isLike: false },
+				{ title: '제목4', contents: '내용4', type: 'notice', isLike: true },
+				{ title: '제목5', contents: '내용5', type: 'news', isLike: false },
+			]);
+			const toggleLike = item => {
+				item.isLike = !item.isLike;
+			};
+			return { contents, posts, toggleLike };
 		},
 	};
 </script>

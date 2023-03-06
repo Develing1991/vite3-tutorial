20bed8601afb83c2da9b4df4481c3f0b8cb0175e
Develing1991 <completed0728@gmail.com>
2023-03-06 22:30:35 +0900

    SFC 여러기능

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,10 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<h5 class="card-title red">Card title</h5>
+			<!-- {{ $style }} -->
+			<!-- <p class="card-text" :class="$style.red"> -->
+			<!-- <p class="card-text" :class="classes.red"> -->
 			<p class="card-text">
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
@@ -12,13 +15,39 @@
 </template>
 
 <script>
+	import { useCssModule, ref } from 'vue';
+
 	console.log('AppCard module');
 	export default {
 		setup() {
-			console.log('AppCard setup');
-			return {};
+			// const style = useCssModule();
+			// console.log(style);
+			// console.log('AppCard setup');
+			const color = ref('red');
+			return { color };
 		},
 	};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+	.red {
+		color: v-bind(color);
+	}
+</style>
+<!-- classes.red -->
+<!-- <style module="classes">
+	.red {
+		color: red;
+	}
+</style> -->
+<!-- $style.red -->
+<!-- <style module>
+	.red {
+		color: red;
+	}
+</style> -->
+<!-- <style scoped>
+	.red {
+		color: red;
+	}
+</style> -->
